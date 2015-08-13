0d7ebcb33a73ec3e8bc4f560a6236a19ed267cc2
Jared King <j@jaredtking.com>
2015-08-13 15:54:44 -0500

    finish update operation - fixes #1

--- lib/invoiced/operations/update.rb
+++ lib/invoiced/operations/update.rb
@@ -1,11 +1,25 @@
 module Invoiced
 	module Operations
 		module Update
-			def save
-    			response = @client.request(:patch, @endpoint, @unsaved)
+			def save(params = {})
+		        update = {}
 
-    			@values = response[:body].dup.merge({:id => self.id})
-    			return response[:code] == 200
+		        @unsaved.each do |k|
+		        	update[k] = @values[k]
+		        end
+
+		        update.merge(params)
+
+				# perform the update if there are any changes
+		        if update.length > 0
+    				response = @client.request(:patch, @endpoint, update)
+
+    				# update the local values with the response
+    				@values = response[:body].dup.merge({:id => self.id})
+    				@unsaved = Set.new
+
+    				return response[:code] == 200
+		        end
     		end
 		end
 	end
